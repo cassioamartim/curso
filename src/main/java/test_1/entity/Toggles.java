1b22007f62885c19c8a7ee424875f4451acb1f18
Cassio <cassioadrianosilvamartim@gmail.com>
2022-02-28 23:17:31 -0300

    Toggle Test

--- src/main/java/test_1/entity/Toggles.java
+++ src/main/java/test_1/entity/Toggles.java
@@ -1,11 +1,13 @@
 package test_1.entity;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Toggles {
     boolean test1 = true;
     boolean test2 = false;
